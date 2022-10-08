10475e342a7ebe5153496f139ae78f2fb36b5785
r3troseer <piagbo3d@gmail.com>
2022-10-08 23:23:16 +0100

    removed unnecessary codes

--- users/serializers.py
+++ users/serializers.py
@@ -132,7 +132,7 @@ class UserLoginSerializer(serializers.Serializer):
         #         ('Phone number is not verified.'))
 
         validated_data['user'] = user
-        print ('login sucessful')
+        print ('login sucessful', phone_no, password)
         return validated_data
 
 
